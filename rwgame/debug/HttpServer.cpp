dc9c4c3b72b0f54aa1dc9c650ba3ee341f685d8a
Timmy Sjöstedt <git@timsjo.se>
2015-04-25 01:08:19 +0200

    Accept incoming connections in a loop

--- rwgame/debug/HttpServer.cpp
+++ rwgame/debug/HttpServer.cpp
@@ -15,22 +15,24 @@ void HttpServer::run()
 
 	std::cout << "STARTING HTTP SERVER" << std::endl;
 
-	sf::TcpSocket client;
-	if (listener.accept(client) == sf::Socket::Done) {
-		std::cout << "New connection from "
-			<< client.getRemoteAddress() << ":" << client.getRemotePort()
-			<< std::endl;
+	while (true) {
+		sf::TcpSocket client;
+		if (listener.accept(client) == sf::Socket::Done) {
+			std::cout << "New connection from "
+				<< client.getRemoteAddress() << ":" << client.getRemotePort()
+				<< std::endl;
 
-		char buf[1024];
-		size_t received;
-		client.receive(buf, 1023, received);
-		buf[received] = '\0';
-		std::cout << "Got " << received << " bytes: " << buf << std::endl;
+			char buf[1024];
+			size_t received;
+			client.receive(buf, 1023, received);
+			buf[received] = '\0';
+			std::cout << "Got " << received << " bytes: " << buf << std::endl;
 
-		std::string response = dispatch();
-		client.send(response.c_str(), response.size());
+			std::string response = dispatch();
+			client.send(response.c_str(), response.size());
 
-		client.disconnect();
+			client.disconnect();
+		}
 	}
 
 	listener.close();
