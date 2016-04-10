d3a95666d44e013bf1449be26a407afbafa3e4cd
Daniel Evans <danharibo@gmail.com>
2016-04-10 18:49:23 +0100

    Add RW_CHECK macro to perform runtime validation in debug

--- rwlib/source/rw/defines.hpp
+++ rwlib/source/rw/defines.hpp
@@ -6,5 +6,12 @@
 #define RW_UNIMPLEMENTED(msg) \
 	std::cout << "Unimplemented: " << msg << std::endl;
 
+#if RW_DEBUG
+#define RW_CHECK(cond, msg) \
+	if(!(cond)) std::cerr << "Check Failed: " << msg << std::endl
+#else
+#define RW_CHECK(cond, msg)
+#endif
+
 
 #endif
