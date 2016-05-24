9fcd3433b3ba229b4a52e83ce264a14a5d868620
Timmy Sjöstedt <git@timsjo.se>
2016-05-24 20:47:32 +0200

    Add OpenAL error checking macro + function

--- rwengine/src/audio/SoundManager.cpp
+++ rwengine/src/audio/SoundManager.cpp
@@ -1,6 +1,16 @@
 #include <audio/SoundManager.hpp>
 #include <audio/MADStream.hpp>
 
+#include <iostream>
+
+void checkALerror(const std::string& file, unsigned int line);
+
+#if RW_DEBUG
+	#define alCheck(stmt) do { stmt; checkALerror(__FILE__, __LINE__); } while(0)
+#else
+	#define alCheck(stmt) stmt
+#endif
+
 SoundManager::SoundManager()
 : backgroundNoise(nullptr)
 {
@@ -40,5 +50,29 @@ void SoundManager::pause(bool p)
 		{
 			backgroundNoise->play();
 		}
+	}
+}
+
+void checkALerror(const std::string& file, unsigned int line)
+{
+	ALenum err = alGetError();
+	if (err != AL_NO_ERROR) {
+		std::cerr << "OpenAL error at " << file << ":" << line << ": ";
+
+		switch (err) {
+		case AL_INVALID_NAME:
+			std::cerr << "Invalid name!";
+			break;
+		case AL_INVALID_VALUE:
+			std::cerr << "Invalid value!";
+			break;
+		case AL_INVALID_OPERATION:
+			std::cerr << "Invalid operation!";
+			break;
+		default:
+			std::cerr << err;
+		}
+
+		std::cerr << std::endl;
 	}
 }
