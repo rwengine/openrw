0ef224df90284efe63b3e0c857448bc5523c73f5
Kamil Rytarowski <n54@gmx.com>
2016-08-13 21:33:04 +0200

    Add preliminary NetBSD support
    The iconv(3) function on NetBSD has a custom prototype due to old mistake in
    the POSIX specification. The issue has been resolved but we keep using original
    form with a constified parameter.

--- rwgame/GameConfig.cpp
+++ rwgame/GameConfig.cpp
@@ -43,7 +43,7 @@ bool GameConfig::isValid()
 
 std::string GameConfig::getDefaultConfigPath()
 {
-#if defined(RW_LINUX) || defined(RW_FREEBSD) || defined(RW_OPENBSD)
+#if defined(RW_LINUX) || defined(RW_FREEBSD) || defined(RW_NETBSD) || defined(RW_OPENBSD)
 	char* config_home = getenv("XDG_CONFIG_HOME");
 	if (config_home != nullptr) {
 		return std::string(config_home) + "/" + kConfigDirectoryName;
@@ -98,4 +98,3 @@ int GameConfig::handler(void* user,
 
 #undef MATCH
 }
-
