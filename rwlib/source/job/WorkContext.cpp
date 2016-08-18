f5568440f45b98df8e9032ad04ceb07bfdbdab95
Daniel Evans <dan@rekt.io>
2016-08-18 23:07:47 +0100

    clang-format files in rwlib/source/job

--- rwlib/source/job/WorkContext.cpp
+++ rwlib/source/job/WorkContext.cpp
@@ -2,40 +2,40 @@
 
 void LoadWorker::start()
 {
-	while( _running ) {
-		_context->workNext();
-		std::this_thread::yield();
-	}
+  while (_running) {
+    _context->workNext();
+    std::this_thread::yield();
+  }
 }
 
 void WorkContext::workNext()
 {
-	WorkJob* j = nullptr;
+  WorkJob* j = nullptr;
 
-	{
-		std::lock_guard<std::mutex> guard( _inMutex );
+  {
+    std::lock_guard<std::mutex> guard(_inMutex);
 
-		if( ! _workQueue.empty() ) {
-			j = _workQueue.front();
-			_workQueue.pop();
-		}
-	}
+    if (!_workQueue.empty()) {
+      j = _workQueue.front();
+      _workQueue.pop();
+    }
+  }
 
-	if( j == nullptr ) return;
+  if (j == nullptr) return;
 
-	j->work();
-	
-	std::lock_guard<std::mutex> guard( _outMutex );
-	_completeQueue.push(j);
+  j->work();
+
+  std::lock_guard<std::mutex> guard(_outMutex);
+  _completeQueue.push(j);
 }
 
 void WorkContext::update()
 {
-	std::lock_guard<std::mutex> guard( _outMutex );
-	while( ! _completeQueue.empty() ) {
-		WorkJob* j = _completeQueue.front(); _completeQueue.pop();
-		j->complete();
-		delete j;
-	}
+  std::lock_guard<std::mutex> guard(_outMutex);
+  while (!_completeQueue.empty()) {
+    WorkJob* j = _completeQueue.front();
+    _completeQueue.pop();
+    j->complete();
+    delete j;
+  }
 }
-
