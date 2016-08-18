204d4b17fe9e4d2329194644c2de0d5a366d686a
Daniel Evans <dan@rekt.io>
2016-08-18 23:07:45 +0100

    clang-format files in rwengine/src/script

--- rwengine/src/script/ScriptDisassembly.hpp
+++ rwengine/src/script/ScriptDisassembly.hpp
@@ -11,44 +11,40 @@ class SCMFile;
 class ScriptDisassembly
 {
 public:
+  enum {
+    /// The opcode's conditional is negated
+    OpcodeFlagNegatedConditional = 1
+  };
 
-	enum
-	{
-		/// The opcode's conditional is negated
-		OpcodeFlagNegatedConditional = 1
-	};
+  /**
+   * Information about a single call to a single opcode and
+   * it's parameters
+   */
+  struct InstructionInfo {
+    /// Numeric Opcode ID
+    SCMOpcode opcode;
+    /// Parameter information
+    SCMParams parameters;
+    uint8_t flags;
+  };
 
-	/**
-	 * Information about a single call to a single opcode and
-	 * it's parameters
-	 */
-	struct InstructionInfo
-	{
-		/// Numeric Opcode ID
-		SCMOpcode opcode;
-		/// Parameter information
-		SCMParams parameters;
-		uint8_t flags;
-	};
+  ScriptDisassembly(SCMOpcodes* codes, SCMFile* scm);
 
-	ScriptDisassembly(SCMOpcodes* codes, SCMFile* scm);
+  /**
+   * Execute the disassembly routine.
+   *
+   * If there is an error during disassembly, an exeption will be
+   * thrown
+   */
+  void disassemble(SCMAddress startAddress);
 
-	/**
-	 * Execute the disassembly routine.
-	 * 
-	 * If there is an error during disassembly, an exeption will be
-	 * thrown
-	 */
-	void disassemble(SCMAddress startAddress);
-
-	std::map<SCMAddress, InstructionInfo>& getInstructions() { return instructions; }
+  std::map<SCMAddress, InstructionInfo>& getInstructions() { return instructions; }
 
 private:
+  SCMOpcodes* codes;
+  SCMFile* scm;
 
-	SCMOpcodes* codes;
-	SCMFile* scm;
-
-	std::map<SCMAddress, InstructionInfo> instructions;
+  std::map<SCMAddress, InstructionInfo> instructions;
 };
 
 #endif
