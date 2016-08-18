95b3e366d3913b224bb78d6021d324b1cd08f7a9
Daniel Evans <dan@rekt.io>
2016-08-18 23:07:47 +0100

    clang-format files in rwlib/source/loaders

--- rwlib/source/loaders/LoaderIMG.hpp
+++ rwlib/source/loaders/LoaderIMG.hpp
@@ -10,57 +10,55 @@
 class LoaderIMGFile
 {
 public:
-	uint32_t offset;
-	uint32_t size;
-	char name[24];
+  uint32_t offset;
+  uint32_t size;
+  char name[24];
 };
 
 /**
-	\class LoaderIMG
-	\brief Parses the structure of GTA .IMG archives and loads the files in it
+    \class LoaderIMG
+    \brief Parses the structure of GTA .IMG archives and loads the files in it
 */
 class LoaderIMG
 {
 public:
-	/// Multiple versions of .IMG files
-	enum Versions
-	{
-		GTAIIIVC, ///< GTA III and GTA VC archives -- only this one is implemented
-		GTASA,
-		GTAIV
-	};
+  /// Multiple versions of .IMG files
+  enum Versions {
+    GTAIIIVC,  ///< GTA III and GTA VC archives -- only this one is implemented
+    GTASA,
+    GTAIV
+  };
 
-	/// Construct
-	LoaderIMG();
+  /// Construct
+  LoaderIMG();
 
-	/// Load the structure of the archive
-	/// Omit the extension in filename so both .dir and .img are loaded when appropriate
-	bool load(const std::string& filename);
+  /// Load the structure of the archive
+  /// Omit the extension in filename so both .dir and .img are loaded when appropriate
+  bool load(const std::string& filename);
 
-	/// Load a file from the archive to memory and pass a pointer to it
-	/// Warning: Please delete[] the memory in the end.
-	/// Warning: Returns NULL (0) if by any reason it can't load the file
-	char* loadToMemory(const std::string& assetname);
+  /// Load a file from the archive to memory and pass a pointer to it
+  /// Warning: Please delete[] the memory in the end.
+  /// Warning: Returns NULL (0) if by any reason it can't load the file
+  char* loadToMemory(const std::string& assetname);
 
-	/// Writes the contents of assetname to filename
-	bool saveAsset(const std::string& assetname, const std::string& filename);
+  /// Writes the contents of assetname to filename
+  bool saveAsset(const std::string& assetname, const std::string& filename);
 
-	/// Get the information of an asset in the examining archive
-	bool findAssetInfo(const std::string& assetname, LoaderIMGFile& out);
+  /// Get the information of an asset in the examining archive
+  bool findAssetInfo(const std::string& assetname, LoaderIMGFile& out);
 
-	/// Get the information of an asset by its index
-	const LoaderIMGFile &getAssetInfoByIndex(size_t index) const;
+  /// Get the information of an asset by its index
+  const LoaderIMGFile& getAssetInfoByIndex(size_t index) const;
 
-	/// Returns the number of asset files in the archive
-	uint32_t getAssetCount() const;
+  /// Returns the number of asset files in the archive
+  uint32_t getAssetCount() const;
 
 private:
-	Versions m_version; ///< Version of this IMG archive
-	uint32_t m_assetCount; ///< Number of assets in the current archive
-	std::string m_archive; ///< Path to the archive being used (no extension)
+  Versions m_version;     ///< Version of this IMG archive
+  uint32_t m_assetCount;  ///< Number of assets in the current archive
+  std::string m_archive;  ///< Path to the archive being used (no extension)
 
-	std::vector<LoaderIMGFile> m_assets; ///< Asset info of the archive
+  std::vector<LoaderIMGFile> m_assets;  ///< Asset info of the archive
 };
 
-
-#endif // LoaderIMG_h__
+#endif  // LoaderIMG_h__
