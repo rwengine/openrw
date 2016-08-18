23cd4799c29af8b04614bc56ab42dcadbc1b06bc
Daniel Evans <dan@rekt.io>
2016-08-18 23:07:47 +0100

    clang-format files in rwviewer/models

--- rwviewer/models/ObjectListModel.cpp
+++ rwviewer/models/ObjectListModel.cpp
@@ -1,89 +1,73 @@
 #include "ObjectListModel.hpp"
 
-ObjectListModel::ObjectListModel(GameData *dat, QObject *parent) :
-	QAbstractTableModel(parent), _gameData( dat )
+ObjectListModel::ObjectListModel(GameData *dat, QObject *parent)
+    : QAbstractTableModel(parent), _gameData(dat)
 {
 }
 
 int ObjectListModel::rowCount(const QModelIndex &parent) const
 {
-	return _gameData->objectTypes.size();
+  return _gameData->objectTypes.size();
 }
 
-int ObjectListModel::columnCount(const QModelIndex &parent) const
-{
-	return 3;
-}
+int ObjectListModel::columnCount(const QModelIndex &parent) const { return 3; }
 
-static std::map<ObjectInformation::ObjectClass, QString> gDataType =
-{
-	{ ObjectInformation::_class("OBJS"), "Object" },
-	{ ObjectInformation::_class("CARS"), "Vehicle" },
-	{ ObjectInformation::_class("PEDS"), "Pedestrian" },
-	{ ObjectInformation::_class("HIER"), "Cutscene" }
-};
+static std::map<ObjectInformation::ObjectClass, QString> gDataType = {
+    {ObjectInformation::_class("OBJS"), "Object"},
+    {ObjectInformation::_class("CARS"), "Vehicle"},
+    {ObjectInformation::_class("PEDS"), "Pedestrian"},
+    {ObjectInformation::_class("HIER"), "Cutscene"}};
 
 QVariant ObjectListModel::data(const QModelIndex &index, int role) const
 {
-	if ( role == Qt::DisplayRole ) {
-		auto id = index.internalId();
-		if( id == -1 ) return QVariant::Invalid;
-		if( index.column() == 0 )
-		{
-			return id;
-		}
-		else if ( index.column() == 1 )
-		{
-			auto object = _gameData->objectTypes[id];
-			if( gDataType[object->class_type].isEmpty() )
-			{
-				return QString("Unknown");
-			}
-			return gDataType[object->class_type];
-		}
-		else if( index.column() == 2 )
-		{
-			auto object = _gameData->objectTypes[id];
-			if(object->class_type == ObjectData::class_id)
-			{
-				auto v = std::static_pointer_cast<ObjectData>(object);
-				return QString::fromStdString(v->modelName);
-			}
-			else if(object->class_type == VehicleData::class_id)
-			{
-				auto v = std::static_pointer_cast<VehicleData>(object);
-				return QString::fromStdString(v->modelName);
-			}
-			else if(object->class_type == CharacterData::class_id)
-			{
-				auto v = std::static_pointer_cast<CharacterData>(object);
-				return QString::fromStdString(v->modelName);
-			}
-		}
-	}
-	return QVariant::Invalid;
+  if (role == Qt::DisplayRole) {
+    auto id = index.internalId();
+    if (id == -1) return QVariant::Invalid;
+    if (index.column() == 0) {
+      return id;
+    } else if (index.column() == 1) {
+      auto object = _gameData->objectTypes[id];
+      if (gDataType[object->class_type].isEmpty()) {
+        return QString("Unknown");
+      }
+      return gDataType[object->class_type];
+    } else if (index.column() == 2) {
+      auto object = _gameData->objectTypes[id];
+      if (object->class_type == ObjectData::class_id) {
+        auto v = std::static_pointer_cast<ObjectData>(object);
+        return QString::fromStdString(v->modelName);
+      } else if (object->class_type == VehicleData::class_id) {
+        auto v = std::static_pointer_cast<VehicleData>(object);
+        return QString::fromStdString(v->modelName);
+      } else if (object->class_type == CharacterData::class_id) {
+        auto v = std::static_pointer_cast<CharacterData>(object);
+        return QString::fromStdString(v->modelName);
+      }
+    }
+  }
+  return QVariant::Invalid;
 }
 
 QVariant ObjectListModel::headerData(int section, Qt::Orientation orientation, int role) const
 {
-	if(role == Qt::DisplayRole && orientation == Qt::Horizontal) {
-		switch(section) {
-		case 0:
-			return "ID";
-		case 1:
-			return "Type";
-		case 2:
-			return "Model";
-		}
-	}
-	return QVariant::Invalid;
+  if (role == Qt::DisplayRole && orientation == Qt::Horizontal) {
+    switch (section) {
+      case 0:
+        return "ID";
+      case 1:
+        return "Type";
+      case 2:
+        return "Model";
+    }
+  }
+  return QVariant::Invalid;
 }
 
 QModelIndex ObjectListModel::index(int row, int column, const QModelIndex &parent) const
 {
-	auto it = _gameData->objectTypes.begin();
-	for(int i = 0; i < row; i++) it++;
-	auto id = it->second->ID;
+  auto it = _gameData->objectTypes.begin();
+  for (int i = 0; i < row; i++) it++;
+  auto id = it->second->ID;
 
-	return hasIndex(row, column, parent) ? createIndex(row, column, id) : QModelIndex();
+  return hasIndex(row, column, parent) ? createIndex(row, column, id) : QModelIndex();
 }
--- rwviewer/models/ObjectListModel.hpp
+++ rwviewer/models/ObjectListModel.hpp
@@ -7,24 +7,25 @@
 
 class ObjectListModel : public QAbstractTableModel
 {
-	Q_OBJECT
+  Q_OBJECT
 
-	GameData* _gameData;
+  GameData* _gameData;
 
 public:
-	explicit ObjectListModel(GameData* gameDat, QObject *parent = 0);
+  explicit ObjectListModel(GameData* gameDat, QObject* parent = 0);
 
-	GameData* gameData() const { return _gameData; }
+  GameData* gameData() const { return _gameData; }
 
-	virtual int rowCount(const QModelIndex& parent = QModelIndex()) const;
+  virtual int rowCount(const QModelIndex& parent = QModelIndex()) const;
 
-	virtual int columnCount(const QModelIndex& parent = QModelIndex()) const;
+  virtual int columnCount(const QModelIndex& parent = QModelIndex()) const;
 
-	virtual QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const;
+  virtual QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const;
 
-	virtual QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
+  virtual QVariant headerData(int section, Qt::Orientation orientation,
+                              int role = Qt::DisplayRole) const;
 
-	QModelIndex index(int row, int column, const QModelIndex &parent) const;
+  QModelIndex index(int row, int column, const QModelIndex& parent) const;
 };
 
-#endif // _OBJECTLISTMODEL_HPP_
+#endif  // _OBJECTLISTMODEL_HPP_
