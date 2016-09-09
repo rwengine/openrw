8534d7ff5dc2d4f9e6ace977f8999de892ddb58d
Daniel Evans <dan@rekt.io>
2016-09-09 21:13:18 +0100

    clang-format files in rwengine/src/dynamics

--- rwengine/src/dynamics/CollisionInstance.hpp
+++ rwengine/src/dynamics/CollisionInstance.hpp
@@ -3,8 +3,8 @@
 #define _COLLISIONINSTANCE_HPP_
 
 #include <btBulletDynamicsCommon.h>
-#include <vector>
 #include <string>
+#include <vector>
 
 class GameObject;
 struct DynamicObjectData;
@@ -13,38 +13,38 @@ struct VehicleHandlingInfo;
 /**
  * @brief CollisionInstance stores bullet body information
  */
-class CollisionInstance
-{
+class CollisionInstance {
 public:
+    CollisionInstance()
+        : m_body(nullptr)
+        , m_vertArray(nullptr)
+        , m_motionState(nullptr)
+        , m_collisionHeight(0.f) {
+    }
 
-	CollisionInstance()
-		: m_body(nullptr)
-		, m_vertArray(nullptr)
-		, m_motionState(nullptr)
-		, m_collisionHeight(0.f)
-	{ }
+    ~CollisionInstance();
 
-	~CollisionInstance();
+    bool createPhysicsBody(GameObject* object, const std::string& modelName,
+                           DynamicObjectData* dynamics = nullptr,
+                           VehicleHandlingInfo* handling = nullptr);
 
-	bool createPhysicsBody(GameObject* object,
-						   const std::string &modelName,
-						   DynamicObjectData* dynamics = nullptr,
-						   VehicleHandlingInfo* handling = nullptr);
+    btRigidBody* getBulletBody() const {
+        return m_body;
+    }
 
-	btRigidBody *getBulletBody() const { return m_body; }
+    float getBoundingHeight() const {
+        return m_collisionHeight;
+    }
 
-	float getBoundingHeight() const { return m_collisionHeight; }
-
-	void changeMass(float newMass);
+    void changeMass(float newMass);
 
 private:
-	btRigidBody* m_body;
-	std::vector<btCollisionShape*> m_shapes;
-	btTriangleIndexVertexArray* m_vertArray;
-	btMotionState* m_motionState;
+    btRigidBody* m_body;
+    std::vector<btCollisionShape*> m_shapes;
+    btTriangleIndexVertexArray* m_vertArray;
+    btMotionState* m_motionState;
 
-	float m_collisionHeight;
-
+    float m_collisionHeight;
 };
 
 #endif
