305737cc3d827675629a0a9f5b0bb17a635be139
Daniel Evans <dan@rekt.io>
2016-09-09 21:13:19 +0100

    clang-format files in rwengine/src/items

--- rwengine/src/items/InventoryItem.hpp
+++ rwengine/src/items/InventoryItem.hpp
@@ -10,49 +10,57 @@ class CharacterObject;
  *
  * Instanciated once per item type
  */
-class InventoryItem
-{
-	int _itemID;
-	int _inventorySlot;
-	int _modelID;
+class InventoryItem {
+    int _itemID;
+    int _inventorySlot;
+    int _modelID;
+
 protected:
-	InventoryItem(int itemID, int invSlot, int model)
-		: _itemID(itemID), _inventorySlot(invSlot), _modelID(model)
-	{}
+    InventoryItem(int itemID, int invSlot, int model)
+        : _itemID(itemID), _inventorySlot(invSlot), _modelID(model) {
+    }
+
 public:
-	virtual ~InventoryItem() {}
+    virtual ~InventoryItem() {
+    }
 
-	/**
-	 * @brief getObject
-	 * @return The ID of the model associated with the item.
-	 */
-	int getModelID() { return _modelID; }
+    /**
+     * @brief getObject
+     * @return The ID of the model associated with the item.
+     */
+    int getModelID() {
+        return _modelID;
+    }
 
-	/**
-	  * @brief getItemID
-	  * @return The index of this item in the item list
-	  */
-	int getItemID() { return _itemID; }
+    /**
+      * @brief getItemID
+      * @return The index of this item in the item list
+      */
+    int getItemID() {
+        return _itemID;
+    }
 
-	/**
-	 * @brief getInventorySlot
-	 * @return The inventory slot number for this item
-	 */
-	int getInventorySlot() const { return _inventorySlot; }
+    /**
+     * @brief getInventorySlot
+     * @return The inventory slot number for this item
+     */
+    int getInventorySlot() const {
+        return _inventorySlot;
+    }
 
-	/**
-	 * @brief primary Implements mouse 1 action
-	 * @param owner The character using this item
-	 */
-	virtual void primary(CharacterObject* owner) = 0;
+    /**
+     * @brief primary Implements mouse 1 action
+     * @param owner The character using this item
+     */
+    virtual void primary(CharacterObject* owner) = 0;
 
-	/**
-	 * @see primary
-	 * @param owner The character using this item
-	 */
-	virtual void secondary(CharacterObject* owner) = 0;
+    /**
+     * @see primary
+     * @param owner The character using this item
+     */
+    virtual void secondary(CharacterObject* owner) = 0;
 
-	constexpr static int NO_INVSLOT = -1;
+    constexpr static int NO_INVSLOT = -1;
 };
 
 #endif
--- rwengine/src/items/WeaponItem.hpp
+++ rwengine/src/items/WeaponItem.hpp
@@ -1,8 +1,8 @@
 #pragma once
 #ifndef _WEAPONITEM_HPP_
 #define _WEAPONITEM_HPP_
-#include <items/InventoryItem.hpp>
 #include <data/WeaponData.hpp>
+#include <items/InventoryItem.hpp>
 #include <memory>
 
 /**
@@ -12,27 +12,29 @@
  * This is instanciated once -per item type-, so state is shared between
  * all instances of the same weapon. Timing is controlled by the CharacterState
  */
-class WeaponItem : public InventoryItem
-{
-	std::shared_ptr<WeaponData> _wepData;
+class WeaponItem : public InventoryItem {
+    std::shared_ptr<WeaponData> _wepData;
 
-	void fireHitscan(CharacterObject* owner);
-	void fireProjectile(CharacterObject* owner);
+    void fireHitscan(CharacterObject* owner);
+    void fireProjectile(CharacterObject* owner);
+
 public:
-	WeaponItem(int itemID, std::shared_ptr<WeaponData> data)
-		: InventoryItem(itemID, data->inventorySlot, data->modelID)
-		, _wepData(data)
-	{}
+    WeaponItem(int itemID, std::shared_ptr<WeaponData> data)
+        : InventoryItem(itemID, data->inventorySlot, data->modelID)
+        , _wepData(data) {
+    }
 
-	void primary(CharacterObject* owner);
+    void primary(CharacterObject* owner);
 
-	void secondary(CharacterObject* owner);
+    void secondary(CharacterObject* owner);
 
-	void fire(CharacterObject* owner);
+    void fire(CharacterObject* owner);
 
-	bool isFiring(CharacterObject* owner);
+    bool isFiring(CharacterObject* owner);
 
-	std::shared_ptr<WeaponData>& getWeaponData() { return _wepData; }
+    std::shared_ptr<WeaponData>& getWeaponData() {
+        return _wepData;
+    }
 };
 
 #endif
