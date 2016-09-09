4ca99c380a87556f6351e506357059bec772592b
Daniel Evans <dan@rekt.io>
2016-09-09 21:13:21 +0100

    clang-format files in rwviewer/models

--- rwviewer/models/DFFFramesTreeModel.hpp
+++ rwviewer/models/DFFFramesTreeModel.hpp
@@ -7,29 +7,32 @@
 class Model;
 class Skeleton;
 
-class DFFFramesTreeModel : public QAbstractItemModel
-{
-	Model* model;
-	Skeleton* skeleton;
+class DFFFramesTreeModel : public QAbstractItemModel {
+    Model* model;
+    Skeleton* skeleton;
+
 public:
-	
-	explicit DFFFramesTreeModel(Model* m, Skeleton* skel, QObject* parent = 0);
-	
+    explicit DFFFramesTreeModel(Model* m, Skeleton* skel, QObject* parent = 0);
+
     virtual int columnCount(const QModelIndex& parent = QModelIndex()) const;
-	
+
     virtual int rowCount(const QModelIndex& parent = QModelIndex()) const;
 
-	virtual QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const;
+    virtual QVariant data(const QModelIndex& index,
+                          int role = Qt::DisplayRole) const;
 
-	virtual bool setData(const QModelIndex& index, const QVariant& value, int role);
+    virtual bool setData(const QModelIndex& index, const QVariant& value,
+                         int role);
 
-	virtual Qt::ItemFlags flags(const QModelIndex& index) const;
+    virtual Qt::ItemFlags flags(const QModelIndex& index) const;
 
-    virtual QModelIndex index(int row, int column, const QModelIndex& parent = QModelIndex()) const;
-	
+    virtual QModelIndex index(int row, int column,
+                              const QModelIndex& parent = QModelIndex()) const;
+
     virtual QModelIndex parent(const QModelIndex& child) const;
-	
-    virtual QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
+
+    virtual QVariant headerData(int section, Qt::Orientation orientation,
+                                int role = Qt::DisplayRole) const;
 };
 
 #endif
