9729ec211121b204e00f7c9979d7340abb6acfb5
Anonymous Maarten <anonymous.maarten@gmail.com>
2017-02-20 20:57:36 +0100

    config: unknown data will be kept in memory and saved onto disk

--- tests/test_config.cpp
+++ tests/test_config.cpp
@@ -3,12 +3,39 @@
 #include <boost/filesystem.hpp>
 #include <boost/test/unit_test.hpp>
 
+#include <boost/property_tree/ini_parser.hpp>
+#include <boost/property_tree/ptree.hpp>
+
 #include <fstream>
 #include <map>
 
+#include "rw/defines.hpp"
+
+namespace pt = boost::property_tree;
 namespace fs = boost::filesystem;
 
-typedef std::map<std::string, std::map<std::string, std::string>> simpleConfig_t;
+typedef std::map<std::string, std::map<std::string, std::string>>
+    simpleConfig_t;
+
+simpleConfig_t readConfig(const std::string &filename) {
+    simpleConfig_t cfg;
+    pt::ptree tree;
+    pt::read_ini(filename, tree);
+
+    for (const auto &section : tree) {
+        for (const auto &subKey : section.second) {
+            cfg[section.first][subKey.first] = subKey.second.data();
+        }
+    }
+    return cfg;
+}
+
+std::string stripWhitespace(const std::string &in) {
+    static const std::string whitespace = " \n\r\t";
+    auto start = in.find_first_not_of(whitespace);
+    auto end = in.find_last_not_of(whitespace) + 1;
+    return std::string(in, start, end - start);
+}
 
 simpleConfig_t getValidConfig() {
     simpleConfig_t result;
@@ -16,14 +43,17 @@ simpleConfig_t getValidConfig() {
     // to test the robustness of the INI parser.
     // Don't change game.path and input.invert_y keys. Tests depend on them.
     result["game"]["path"] = "\t/dev/test  \t \r\n";
-    result["game"]["\tlanguage\t "] = "      american ;american english french german italian spanish.";
-    result["input"]["invert_y"] = "1 #values != 0 enable input inversion. Optional.";
+    result["game"]["\tlanguage\t "] =
+        "      american ;american english french german italian spanish.";
+    result["input"]["invert_y"] =
+        "1 #values != 0 enable input inversion. Optional.";
     return result;
 }
 
 std::ostream &operator<<(std::ostream &os, const simpleConfig_t &config) {
     for (auto &section : config) {
-        os << "[" << section.first << "]" << "\n";
+        os << "[" << section.first << "]"
+           << "\n";
         for (auto &keyValue : section.second) {
             os << keyValue.first << "=" << keyValue.second << "\n";
         }
@@ -59,10 +89,11 @@ public:
         return this->m_path.parent_path().string();
     }
     void change_perms_readonly() {
-        fs::permissions(this->m_path, fs::perms::owner_read
-            | fs::perms::group_read | fs::perms::others_read);
+        fs::permissions(this->m_path, fs::perms::owner_read |
+                                          fs::perms::group_read |
+                                          fs::perms::others_read);
     }
-    template<typename T>
+    template <typename T>
     bool append(T t) {
         // Append argument at the end of the file.
         // File is open/closes repeatedly. Not optimal.
@@ -71,7 +102,7 @@ public:
         ofs.close();
         return ofs.good();
     }
-    template<typename T>
+    template <typename T>
     bool write(T t) {
         // Write the argument to the file, discarding all contents.
         // File is open/closes repeatedly. Not optimal.
@@ -80,14 +111,30 @@ public:
         ofs.close();
         return ofs.good();
     }
+
 private:
     static fs::path getRandomFilePath() {
-        return fs::unique_path(fs::temp_directory_path() / "openrw_test_%%%%%%%%%%%%%%%%");
+        return fs::unique_path(fs::temp_directory_path() /
+                               "openrw_test_%%%%%%%%%%%%%%%%");
     }
     fs::path m_path;
 };
 
 BOOST_AUTO_TEST_SUITE(ConfigTests)
+
+BOOST_AUTO_TEST_CASE(test_stripWhitespace) {
+    std::map<std::string, std::string> map;
+    map["abc"] = "abc";
+    map["\tabc"] = "abc";
+    map["abc\t"] = "abc";
+    map[" abc"] = "abc";
+    map["abc "] = "abc";
+    map[" abc "] = "abc";
+    map["  abc  "] = "abc";
+    for (const auto &keyValue : map) {
+        BOOST_CHECK_EQUAL(keyValue.second, stripWhitespace(keyValue.first));
+    }
+}
 
 BOOST_AUTO_TEST_CASE(test_TempFile) {
     // Check the behavior of TempFile
@@ -126,8 +173,10 @@ BOOST_AUTO_TEST_CASE(test_config_valid) {
     GameConfig config(tempFile.filename(), tempFile.dirname());
 
     BOOST_CHECK(config.isValid());
-    BOOST_CHECK_EQUAL(config.getParseResult().type(), GameConfig::ParseResult::ErrorType::GOOD);
-    BOOST_CHECK_EQUAL(config.getParseResult().getKeysRequiredMissing().size(), 0);
+    BOOST_CHECK_EQUAL(config.getParseResult().type(),
+                      GameConfig::ParseResult::ErrorType::GOOD);
+    BOOST_CHECK_EQUAL(config.getParseResult().getKeysRequiredMissing().size(),
+                      0);
     BOOST_CHECK_EQUAL(config.getParseResult().getKeysInvalidData().size(), 0);
 
     BOOST_CHECK_EQUAL(config.getGameDataPath(), "/dev/test");
@@ -147,8 +196,10 @@ BOOST_AUTO_TEST_CASE(test_config_valid_modified) {
     GameConfig config(tempFile.filename(), tempFile.dirname());
 
     BOOST_CHECK(config.isValid());
-    BOOST_CHECK_EQUAL(config.getParseResult().type(), GameConfig::ParseResult::ErrorType::GOOD);
-    BOOST_CHECK_EQUAL(config.getParseResult().getKeysRequiredMissing().size(), 0);
+    BOOST_CHECK_EQUAL(config.getParseResult().type(),
+                      GameConfig::ParseResult::ErrorType::GOOD);
+    BOOST_CHECK_EQUAL(config.getParseResult().getKeysRequiredMissing().size(),
+                      0);
     BOOST_CHECK_EQUAL(config.getParseResult().getKeysInvalidData().size(), 0);
 
     BOOST_CHECK_EQUAL(config.getInputInvertY(), false);
@@ -176,6 +227,55 @@ BOOST_AUTO_TEST_CASE(test_config_save) {
 
     GameConfig config2(tempFile.filename(), tempFile.dirname());
     BOOST_CHECK_EQUAL(config2.getGameDataPath(), "Liberty City");
+
+    simpleConfig_t cfg2 = readConfig(tempFile.path());
+    BOOST_CHECK_EQUAL(cfg2["game"]["path"], "Liberty City");
+}
+
+BOOST_AUTO_TEST_CASE(test_config_valid_unknown_keys) {
+    // Test reading a valid modified configuration file with unknown data
+    auto cfg = getValidConfig();
+    cfg["game"]["unknownkey"] = "descartes";
+    cfg["dontknow"]["dontcare"] = "\t$%!$8847  %%$  ";
+
+    TempFile tempFile;
+    tempFile.append(cfg);
+
+    GameConfig config(tempFile.filename(), tempFile.dirname());
+
+    BOOST_CHECK(config.isValid());
+
+    const auto &unknownData = config.getParseResult().getUnknownData();
+
+    BOOST_CHECK_EQUAL(unknownData.size(), 2);
+
+    BOOST_CHECK_EQUAL(unknownData.count("game.unknownkey"), 1);
+    BOOST_CHECK_EQUAL(unknownData.at("game.unknownkey"),
+                      stripWhitespace(cfg["game"]["unknownkey"]));
+
+    BOOST_CHECK_EQUAL(unknownData.count("dontknow.dontcare"), 1);
+    BOOST_CHECK_EQUAL(unknownData.at("dontknow.dontcare"),
+                      stripWhitespace(cfg["dontknow"]["dontcare"]));
+
+    BOOST_CHECK_EQUAL(unknownData.count("game.path"), 0);
+
+    tempFile.remove();
+    config.saveConfig();
+
+    GameConfig config2(tempFile.filename(), tempFile.dirname());
+    const auto &unknownData2 = config2.getParseResult().getUnknownData();
+
+    BOOST_CHECK_EQUAL(unknownData2.size(), 2);
+
+    BOOST_CHECK_EQUAL(unknownData2.count("game.unknownkey"), 1);
+    BOOST_CHECK_EQUAL(unknownData2.at("game.unknownkey"),
+                      stripWhitespace(cfg["game"]["unknownkey"]));
+
+    BOOST_CHECK_EQUAL(unknownData2.count("dontknow.dontcare"), 1);
+    BOOST_CHECK_EQUAL(unknownData2.at("dontknow.dontcare"),
+                      stripWhitespace(cfg["dontknow"]["dontcare"]));
+
+    BOOST_CHECK_EQUAL(unknownData2.count("game.path"), 0);
 }
 
 BOOST_AUTO_TEST_CASE(test_config_save_readonly) {
@@ -191,7 +291,8 @@ BOOST_AUTO_TEST_CASE(test_config_save_readonly) {
 
     auto writeResult = config.saveConfig();
     BOOST_CHECK(!writeResult.isValid());
-    BOOST_CHECK_EQUAL(writeResult.type(), GameConfig::ParseResult::ErrorType::INVALIDOUTPUTFILE);
+    BOOST_CHECK_EQUAL(writeResult.type(),
+                      GameConfig::ParseResult::ErrorType::INVALIDOUTPUTFILE);
 }
 
 BOOST_AUTO_TEST_CASE(test_config_valid_default) {
@@ -209,6 +310,22 @@ BOOST_AUTO_TEST_CASE(test_config_valid_default) {
     BOOST_CHECK(config.isValid());
 }
 
+BOOST_AUTO_TEST_CASE(test_config_invalid_emptykey) {
+    // Test duplicate keys in invalid configuration file
+    auto cfg = getValidConfig();
+    cfg["game"][""] = "0";
+
+    TempFile tempFile;
+    tempFile.append(cfg);
+
+    GameConfig config(tempFile.filename(), tempFile.dirname());
+
+    BOOST_CHECK(!config.isValid());
+    const auto &parseResult = config.getParseResult();
+    BOOST_CHECK_EQUAL(parseResult.type(),
+                      GameConfig::ParseResult::ErrorType::INVALIDINPUTFILE);
+}
+
 BOOST_AUTO_TEST_CASE(test_config_invalid_duplicate) {
     // Test duplicate keys in invalid configuration file
     auto cfg = getValidConfig();
@@ -221,7 +338,8 @@ BOOST_AUTO_TEST_CASE(test_config_invalid_duplicate) {
 
     BOOST_CHECK(!config.isValid());
     const auto &parseResult = config.getParseResult();
-    BOOST_CHECK_EQUAL(parseResult.type(), GameConfig::ParseResult::ErrorType::INVALIDINPUTFILE);
+    BOOST_CHECK_EQUAL(parseResult.type(),
+                      GameConfig::ParseResult::ErrorType::INVALIDINPUTFILE);
 }
 
 BOOST_AUTO_TEST_CASE(test_config_invalid_required_missing) {
@@ -237,7 +355,8 @@ BOOST_AUTO_TEST_CASE(test_config_invalid_required_missing) {
     BOOST_CHECK(!config.isValid());
 
     const auto &parseResult = config.getParseResult();
-    BOOST_CHECK_EQUAL(parseResult.type(), GameConfig::ParseResult::ErrorType::INVALIDCONTENT);
+    BOOST_CHECK_EQUAL(parseResult.type(),
+                      GameConfig::ParseResult::ErrorType::INVALIDCONTENT);
 
     BOOST_CHECK_EQUAL(parseResult.getKeysRequiredMissing().size(), 1);
     BOOST_CHECK_EQUAL(parseResult.getKeysInvalidData().size(), 0);
@@ -248,7 +367,7 @@ BOOST_AUTO_TEST_CASE(test_config_invalid_required_missing) {
 BOOST_AUTO_TEST_CASE(test_config_invalid_wrong_type) {
     // Test wrong data type
     auto cfg = getValidConfig();
-    cfg["input"]["invert_y"]="d";
+    cfg["input"]["invert_y"] = "d";
 
     TempFile tempFile;
     tempFile.append(cfg);
@@ -258,7 +377,8 @@ BOOST_AUTO_TEST_CASE(test_config_invalid_wrong_type) {
     BOOST_CHECK(!config.isValid());
 
     const auto &parseResult = config.getParseResult();
-    BOOST_CHECK_EQUAL(parseResult.type(), GameConfig::ParseResult::ErrorType::INVALIDCONTENT);
+    BOOST_CHECK_EQUAL(parseResult.type(),
+                      GameConfig::ParseResult::ErrorType::INVALIDCONTENT);
 
     BOOST_CHECK_EQUAL(parseResult.getKeysRequiredMissing().size(), 0);
     BOOST_CHECK_EQUAL(parseResult.getKeysInvalidData().size(), 1);
@@ -268,7 +388,8 @@ BOOST_AUTO_TEST_CASE(test_config_invalid_wrong_type) {
 
 BOOST_AUTO_TEST_CASE(test_config_invalid_empty) {
     // Test reading empty configuration file
-    // An empty file has a valid data structure, but has missing keys and is thus invalid.
+    // An empty file has a valid data structure, but has missing keys and is
+    // thus invalid.
     TempFile tempFile;
     tempFile.touch();
     BOOST_CHECK(tempFile.exists());
@@ -278,7 +399,8 @@ BOOST_AUTO_TEST_CASE(test_config_invalid_empty) {
     BOOST_CHECK(!config.isValid());
 
     const auto &parseResult = config.getParseResult();
-    BOOST_CHECK_EQUAL(parseResult.type(), GameConfig::ParseResult::ErrorType::INVALIDCONTENT);
+    BOOST_CHECK_EQUAL(parseResult.type(),
+                      GameConfig::ParseResult::ErrorType::INVALIDCONTENT);
     BOOST_CHECK_GE(parseResult.getKeysRequiredMissing().size(), 1);
 }
 
@@ -292,7 +414,8 @@ BOOST_AUTO_TEST_CASE(test_config_invalid_nonexisting) {
     BOOST_CHECK(!config.isValid());
 
     const auto &parseResult = config.getParseResult();
-    BOOST_CHECK_EQUAL(parseResult.type(), GameConfig::ParseResult::ErrorType::INVALIDINPUTFILE);
+    BOOST_CHECK_EQUAL(parseResult.type(),
+                      GameConfig::ParseResult::ErrorType::INVALIDINPUTFILE);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
