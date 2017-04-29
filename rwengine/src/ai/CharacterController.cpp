dc3e2b90a7b6baf8ac3b7307f041e569916f9992
Aldoxtor <sotliseri3@gmail.com>
2017-04-29 19:49:14 +0000

    properly skip activity

--- rwengine/src/ai/CharacterController.cpp
+++ rwengine/src/ai/CharacterController.cpp
@@ -40,8 +40,10 @@ void CharacterController::skipActivity() {
     // vehicle
     // or jumping.
     if (getCurrentActivity() != nullptr &&
-        getCurrentActivity()->canSkip(character, this))
+        getCurrentActivity()->canSkip(character, this)) {
+        character->activityFinished();
         setActivity(nullptr);
+        }
 }
 
 void CharacterController::setNextActivity(
