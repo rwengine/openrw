1af5d74faa83a8e3fe333e6a096516410e995469
Anonymous Maarten <anonymous.maarten@gmail.com>
2017-08-30 20:30:43 +0200

    tests: Include main test function from boost/test/unit_test.hpp
    See https://bugzilla.redhat.com/show_bug.cgi?id=1446852

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,5 +1,5 @@
 #define BOOST_TEST_MODULE openrw
-#include <boost/test/included/unit_test.hpp>
+#include <boost/test/unit_test.hpp>
 #include "test_Globals.hpp"
 
 std::ostream& operator<<(std::ostream& stream, const glm::vec3& v) {
