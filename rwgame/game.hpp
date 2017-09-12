853c1a01b27eda0586ed932ebe9d539b8e99ce76
Anonymous Maarten <anonymous.maarten@gmail.com>
2017-09-12 02:02:26 +0200

    rwgame: remove unused unimplemented function definitions

--- rwgame/game.hpp
+++ rwgame/game.hpp
@@ -1,19 +1,6 @@
 #ifndef GAME_HPP
 #define GAME_HPP
 
-#include <engine/GameWorld.hpp>
-#include <objects/GameObject.hpp>
-
-constexpr double PiOver180 = 3.1415926535897932384626433832795028 / 180;
-
-// TODO: Move all of this stuff so it's not just lying around.
-
-bool hitWorldRay(glm::vec3& hit, glm::vec3& normal,
-                 GameObject** object = nullptr);
-bool hitWorldRay(const glm::vec3& start, const glm::vec3& direction,
-                 glm::vec3& hit, glm::vec3& normal,
-                 GameObject** object = nullptr);
-
 #define GAME_TIMESTEP (1.f / 30.f)
 
 #endif  // GAME_HPP
