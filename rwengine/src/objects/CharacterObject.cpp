caa557557357143f8489cc6668c3cd0ca8e7ec52
Filip Gawin <filip.gawin@zoho.com>
2017-10-19 22:13:55 +0200

    Add check reading animation ptr

--- rwengine/src/objects/CharacterObject.cpp
+++ rwengine/src/objects/CharacterObject.cpp
@@ -196,6 +196,7 @@ glm::vec3 CharacterObject::updateMovementAnimation(float dt) {
         if (it != movementAnimation->bones.end()) {
             AnimationBone* rootBone = it->second;
             float step = dt;
+            RW_CHECK(animator->getAnimation(AnimIndexMovement), "Failed to read animation using index " << AnimIndexMovement);
             const float duration =
                 animator->getAnimation(AnimIndexMovement)->duration;
             float animTime =
