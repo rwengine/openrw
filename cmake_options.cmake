a40a2706a9c7aff4ed883659a56354c3b31d1b32
Anonymous Maarten <anonymous.maarten@gmail.com>
2017-10-29 07:37:18 +0100

    cmake: add support for include-what-you-use to check #include's
    The output is currently very verbose and includes many false positives.
    Adding a mapping file should solve this.
    
    See
    https://github.com/include-what-you-use/include-what-you-use/blob/master/docs/IWYUMappings.md

--- cmake_options.cmake
+++ cmake_options.cmake
@@ -15,8 +15,10 @@ set(BIN_DIR "bin" CACHE STRING "Prefix subdirectory to put the binaries in.")
 set(DOC_DIR "share/doc/openrw" CACHE STRING "Prefix subdirectory to put the documentation in.")
 
 if(NOT CMAKE_BUILD_TYPE)
-  set(CMAKE_BUILD_TYPE Release CACHE STRING "Choose the type of build, options are: Debug Release")
+    set(CMAKE_BUILD_TYPE Release CACHE STRING "Choose the type of build, options are: Debug Release")
 endif()
+
+option(CHECK_INCLUDES "Analyze #includes in C and C++ source files")
 
 option(TEST_COVERAGE "Enable coverage analysis (implies CMAKE_BUILD_TYPE=Debug)")
 option(SEPERATE_TEST_SUITES "Add each test suite as seperate test")
