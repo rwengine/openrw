56c27fce551ef9bde5b0a6587ae0c45669ca186e
NFSMONSTR <nfsmonstr@gmail.com>
2018-06-09 18:23:16 +0300

    Don't draw asterisks in subtitles (#510)
    Don't draw asterisks in subtitles

--- rwengine/src/render/TextRenderer.cpp
+++ rwengine/src/render/TextRenderer.cpp
@@ -141,6 +141,9 @@ void TextRenderer::setFontTexture(int index, const std::string& texture) {
 
 void TextRenderer::renderText(const TextRenderer::TextInfo& ti,
                               bool forceColour) {
+    if (ti.text.empty() || ti.text[0] == '*')
+        return;
+
     renderer->getRenderer()->pushDebugGroup("Text");
     renderer->getRenderer()->useProgram(textShader.get());
 
