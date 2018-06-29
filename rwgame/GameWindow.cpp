93c55057c89f53ef340f07c3fd5ca3fe0ac6ea82
Filip Gawin <filip.gawin@zoho.com>
2018-06-29 23:15:23 +0200

    Add an SDL window icon to the executable, thx @vflyson

--- rwgame/GameWindow.cpp
+++ rwgame/GameWindow.cpp
@@ -31,11 +31,36 @@ void GameWindow::create(const std::string& title, size_t w, size_t h,
         throw std::runtime_error("SDL_GL_CreateContext failed: " + sdlErrorStr);
     }
 
+    // This part sets an embedded icon to the window
+    // The source "image" is a 32-bit RGBA buffer exported from GIMP
+    // The full name of the format is "GIMP RGBA C-Source image dump"
+#include "WindowIcon.hpp"
+    Uint32 rmask, gmask, bmask, amask;
+#if SDL_BYTEORDER == SDL_BIG_ENDIAN
+    // Big Endian
+    rmask = 0xff000000;
+    gmask = 0x00ff0000;
+    bmask = 0x0000ff00;
+    amask = 0x000000ff;
+#else
+    // Little Endian
+    rmask = 0x000000ff;
+    gmask = 0x0000ff00;
+    bmask = 0x00ff0000;
+    amask = 0xff000000;
+#endif
+    icon = SDL_CreateRGBSurfaceFrom((void*)windowIconData,
+                                    windowIconWidth, windowIconHeight,
+                                    32, windowIconWidth * (32 / 8),
+                                    rmask, gmask, bmask, amask);
+    SDL_SetWindowIcon(window, icon);
+
     SDL_ShowWindow(window);
 }
 
 void GameWindow::close() {
     SDL_GL_DeleteContext(glcontext);
+    SDL_FreeSurface(icon);
     SDL_DestroyWindow(window);
 
     window = nullptr;
