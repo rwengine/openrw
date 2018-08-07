7938f8678fc6b28c59a40789bab5db6c32fdb153
Daniel Evans <dan@devans.xyz>
2018-08-07 01:18:01 +0100

    Add LoaderIPL tests for inst section

--- tests/test_LoaderIPL.cpp
+++ tests/test_LoaderIPL.cpp
@@ -1,5 +1,6 @@
 #include <boost/test/unit_test.hpp>
 #include <loaders/LoaderIPL.hpp>
+#include <data/InstanceData.hpp>
 #include "test_Globals.hpp"
 
 namespace {
@@ -7,6 +8,12 @@ constexpr auto kIPLTestData = R"(
 zone
 ZONE_A, 1, -100.0, -200.00, -100.0, 100.0, 1000.0, 100.0, 1
 ZONE_B, 0,  200.0, 10.0, -100.0, 100.0, 1000.0, 100.0, 2
+end
+
+inst
+101, ModelA, 10.0, 12.0, 5.0, 1, 1, 1, 0, 0, -0.70, 0.70
+112, ModelB, 10.0, 12.0, 5.0, 1, 1, 1, 0, 0, -0.70, 0.70
+112, ModelB, 11.0, 12.0, 5.0, 1, 1, 1, 0, 0, 0, 1
 end
 )";
 }
@@ -21,7 +28,6 @@ BOOST_FIXTURE_TEST_SUITE(LoaderIPLTests, WithLoaderIPL)
 
 BOOST_AUTO_TEST_CASE(zone_count_is_correct) {
     BOOST_REQUIRE(loader.load(test_data_stream));
-
     BOOST_TEST(loader.zones.size() == 2);
 }
 
@@ -33,6 +39,23 @@ BOOST_AUTO_TEST_CASE(zone_data_is_correct) {
     BOOST_TEST(zone1.type == 0);
     BOOST_TEST(zone1.min.x == 200.0f);
     BOOST_TEST(zone1.island == 2);
+}
+
+BOOST_AUTO_TEST_CASE(instance_count_is_correct) {
+    BOOST_REQUIRE(loader.load(test_data_stream));
+    BOOST_TEST(loader.m_instances.size() == 3);
+}
+
+BOOST_AUTO_TEST_CASE(instance_data_is_correct) {
+    BOOST_REQUIRE(loader.load(test_data_stream));
+
+    auto& instance = loader.m_instances[1];
+    BOOST_TEST(instance->id == 112);
+    BOOST_TEST(instance->model == "ModelB");
+    BOOST_TEST(instance->pos.x == 10.0f);
+    BOOST_TEST(instance->pos.y == 12.0f);
+    BOOST_TEST(instance->pos.z == 5.0f);
+    BOOST_TEST(instance->rot.x == 0.0f);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
