fa1ff787e556dceeaabafaa335525f74f58bda9a
Anonymous Maarten <anonymous.maarten@gmail.com>
2018-08-13 20:06:06 +0200

    ci: add debug build

--- scripts/docker/docker_travis.sh
+++ scripts/docker/docker_travis.sh
@@ -8,7 +8,17 @@ docker=$1
 "$curdir/docker_tool.py" build -d "$docker" -t openrw_build_image
 
 # Start docker container + add travis user
-"$curdir/docker_tool.py" create -t openrw_build_image -n openrw_builder -U travis -e NAME_SUFFIX=$NAME_SUFFIX XDG_RUNTIME_DIR=/tmp TRAVIS=$TRAVIS TRAVIS_COMMIT=$TRAVIS_COMMIT TRAVIS_EVENT_TYPE=$TRAVIS_EVENT_TYPE TRAVIS_REPO_SLUG=$TRAVIS_REPO_SLUG TRAVIS_BRANCH=$TRAVIS_BRANCH USE_CONAN=$USE_CONAN
+"$curdir/docker_tool.py" create -t openrw_build_image -n openrw_builder -U travis \
+    -e \
+        NAME_SUFFIX=$NAME_SUFFIX \
+        TRAVIS=$TRAVIS \
+        TRAVIS_COMMIT=$TRAVIS_COMMIT \
+        TRAVIS_EVENT_TYPE=$TRAVIS_EVENT_TYPE \
+        TRAVIS_REPO_SLUG=$TRAVIS_REPO_SLUG \
+        TRAVIS_BRANCH=$TRAVIS_BRANCH \
+        USE_CONAN=$USE_CONAN \
+        DEBUG=$DEBUG \
+        XDG_RUNTIME_DIR=/tmp
 
 # execute test
 "$curdir/docker_tool.py" exec -n openrw_builder -U travis -- /bin/bash -c "ctest -VV -S /src/cmake/ctest/script_ci.ctest -VV"
