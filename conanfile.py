6f89f444780b2bcb160cca011f270d6db7062c00
Filip Gawin <filip.gawin@zoho.com>
2018-10-16 10:30:11 +0200

    Update conan dependency - glm

--- conanfile.py
+++ conanfile.py
@@ -31,7 +31,7 @@ class OpenrwConan(ConanFile):
         'game': (
             'openal/1.18.2@bincrafters/stable',
             'bullet/2.87@bincrafters/stable',
-            'glm/0.9.9.0@g-truc/stable',
+            'glm/0.9.9.1@g-truc/stable',
             'ffmpeg/4.0@bincrafters/stable',
             'sdl2/2.0.8@bincrafters/stable',
             'boost/1.67.0@conan/stable',
