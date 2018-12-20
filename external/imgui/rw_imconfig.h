0e5f1e5b5fb54d6630f196f0aed84f57695c0515
Anonymous Maarten <anonymous.maarten@gmail.com>
2018-12-20 20:30:22 +0100

    cmake+imgui: add imgui submodule

---
+++ external/imgui/rw_imconfig.h
@@ -0,0 +1,9 @@
+#ifndef RW_IMCONFIG_H
+#define RW_IMCONFIG_H
+
+// Disable imgui assertions when not in debug mode
+#ifndef RW_DEBUG
+#define IM_ASSERT(MSG) //FIXME(madebr): remove comment
+#endif
+
+#endif // RW_IMCONFIG_H
