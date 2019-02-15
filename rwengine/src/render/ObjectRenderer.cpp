342e3c8f830156b416b04144b794dd30dc1265ac
Artem Popov <Deviling42@gmail.com>
2019-02-15 17:49:44 -0500

    Pickups speed adjusted. Closes #694 (#701)
    * Pickups speed adjusted
    
    * requested changes
    
    * Variable name adjusted

--- rwengine/src/render/ObjectRenderer.cpp
+++ rwengine/src/render/ObjectRenderer.cpp
@@ -307,10 +307,11 @@ void ObjectRenderer::renderVehicle(VehicleObject* vehicle,
 
 void ObjectRenderer::renderPickup(PickupObject* pickup, RenderList& outList) {
     if (!pickup->isEnabled()) return;
-
+    
+    static constexpr float kRotationSpeedCoeff = 3.0f;
     glm::mat4 modelMatrix =
         glm::translate(glm::mat4(1.0f), pickup->getPosition());
-    modelMatrix = glm::rotate(modelMatrix, m_world->getGameTime(),
+    modelMatrix = glm::rotate(modelMatrix, m_world->getGameTime() * kRotationSpeedCoeff,
                               glm::vec3(0.f, 0.f, 1.f));
 
     auto odata = pickup->getModelInfo<SimpleModelInfo>();
