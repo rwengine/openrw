ef77d401d52c1efd8cd23c5b43691b772193770d
Filip Gawin <filip.gawin@zoho.com>
2019-04-28 18:58:29 +0200

    RE missing stuff from VehicleObject

---
+++ rwengine/src/objects/Vehicle/AutoPilot.hpp
@@ -0,0 +1,105 @@
+﻿#ifndef _RWENGINE_AUTOPILOT_HPP_
+#define _RWENGINE_AUTOPILOT_HPP_
+
+#include <glm/vec3.hpp>
+
+#include <array>
+#include <cstdint>
+
+enum DrivingStyle : int8_t {
+    DRIVINGMODE_STOPFORCARS = 0x0,
+    DRIVINGMODE_SLOWDOWNFORCARS = 0x1,
+    DRIVINGMODE_AVOIDCARS = 0x2,
+    DRIVINGMODE_PLOUGHTHROUGH = 0x3,
+    DRIVINGMODE_STOPFORCARS_IGNORELIGHTS = 0x4,
+};
+
+enum CarMission : int8_t {
+    MISSION_NONE = 0x0,
+    MISSION_CRUISE = 0x1,
+    MISSION_RAMPLAYER_FARAWAY = 0x2,
+    MISSION_RAMPLAYER_CLOSE = 0x3,
+    MISSION_BLOCKPLAYER_FARAWAY = 0x4,
+    MISSION_BLOCKPLAYER_CLOSE = 0x5,
+    MISSION_BLOCKPLAYER_HANDBRAKESTOP = 0x6,
+    MISSION_WAITFORDELETION = 0x7,
+    MISSION_GOTOCOORDS = 0x8,
+    MISSION_GOTOCOORDS_STRAIGHT = 0x9,
+    MISSION_EMERGENCYVEHICLE_STOP = 0xA,
+    MISSION_STOP_FOREVER = 0xB,
+    MISSION_GOTOCOORDS_ACCURATE = 0xC,
+    MISSION_GOTO_COORDS_STRAIGHT_ACCURATE = 0xD,
+    MISSION_GOTOCOORDS_ASTHECROWSWIMS = 0xE,
+    MISSION_RAMCAR_FARAWAY = 0xF,
+    MISSION_RAMCAR_CLOSE = 0x10,
+    MISSION_BLOCKCAR_FARAWAY = 0x11,
+    MISSION_BLOCKCAR_CLOSE = 0x12,
+    MISSION_BLOCKCAR_HANDBRAKESTOP = 0x13,
+    MISSION_HELI_FLYTOCOORS = 0x14,
+    MISSION_ATTACKPLAYER = 0x15,
+    MISSION_PLANE_FLYTOCOORS = 0x16,
+    MISSION_HELI_LAND = 0x17,
+    MISSION_SLOWLY_DRIVE_TOWARDS_PLAYER_1 = 0x18,
+    MISSION_SLOWLY_DRIVE_TOWARDS_PLAYER_2 = 0x19,
+    MISSION_BLOCKPLAYER_FORWARDANDBACK = 0x1A,
+};
+
+enum TempAction : int8_t {
+    TEMPACT_NONE = 0x0,
+    TEMPACT_WAIT = 0x1,
+    TEMPACT_REVERSE = 0x2,
+    TEMPACT_HANDBRAKETURNLEFT = 0x3,
+    TEMPACT_HANDBRAKETURNRIGHT = 0x4,
+    TEMPACT_HANDBRAKESTRAIGHT = 0x5,
+    TEMPACT_TURNLEFT = 0x6,
+    TEMPACT_TURNRIGHT = 0x7,
+    TEMPACT_GOFORWARD = 0x8,
+    TEMPACT_SWERVELEFT = 0x9,
+    TEMPACT_SWERVERIGHT = 0xA,
+};
+
+class VehicleObjet;
+struct AIGraphNode;
+
+struct AutoPilot {
+    // RE stuff
+    // Please don't modify order, add or remove objects here!
+    // If member is unknown and unused, then it should be also commented
+    // If member is replaced by something else, then you should point/link to
+    // that "something else" using "see ..."
+
+    int currentNodeId;           // int m_nCurrentNodeId;
+    int nextNodeId;              // int m_nNextNodeId;
+    int previousNodeId;          // int m_nPreviousNodeId;
+    int totalSpeedScaleFactor;   // int m_nTotalSpeedScaleFactor;
+    int speedScaleFactor;        // int m_nSpeedScaleFactor;
+    int currentCarPathLink;      // int m_nCurrentCarPathLink;
+    int nextCarPathLink;         // int m_nNextCarPathLink;
+    int previousCarPathLink;     // int m_nPreviousCarPathLink;
+    int stopCommandTime;         // int m_nStopCommandTime;
+    int lastCommandTime;         // int m_nLastCommandTime;
+    char previousDirection;      // char m_bPreviousDirection;
+    char currentDirecton;        // char m_bCurrentDirecton;
+    char nextDirection;          // char m_bNextDirection;
+    char previousPathDirection;  // char m_bPreviousPathDirection;
+    char currentPathDirection;   // char m_bCurrentPathDirection;
+    DrivingStyle drivingStyle;   // eDrivingStyle m_eDrivingStyle;
+    CarMission carMission;       // eCarMission m_eCarMission;
+    TempAction tempAction;       // eTempAction m_eTempAction;
+    int timeSimpleAction;        // int m_nTimeSimpleAction;
+    float speedChange;           // float m_fSpeedChange;
+    char maxSpeed;               // char m_bMaxSpeed;
+    uint8_t flags;               // uint8_t m_bFlags;
+    // char field_358;
+    // char field_359;
+    glm::vec3 vecGotoCoors;                 // CVec m_vecGotoCoors;
+    std::array<AIGraphNode*, 8> pathNodes;  // CPathNode* m_pPathNode[8];
+    // int16_t m_nTotalPathNodes;
+    // char field_406;
+    // char field_407;
+    VehicleObjet* target;  // CVehicle *m_pTarget;
+
+    // end of RE stuff
+};
+
+#endif
--- rwengine/src/objects/VehicleObject.hpp
+++ rwengine/src/objects/VehicleObject.hpp
@@ -1,4 +1,4 @@
-#ifndef _RWENGINE_VEHICLEOBJECT_HPP_
+﻿#ifndef _RWENGINE_VEHICLEOBJECT_HPP_
 #define _RWENGINE_VEHICLEOBJECT_HPP_
 
 #include <cstddef>
@@ -20,8 +20,8 @@
 
 #include <data/ModelData.hpp>
 #include <objects/GameObject.hpp>
+#include <objects/Vehicle/AutoPilot.hpp>
 #include <objects/VehicleInfo.hpp>
-
 
 class Atomic;
 class CharacterObject;
@@ -34,6 +34,41 @@ class btRigidBody;
 class btHingeConstraint;
 class btMotionState;
 struct btVehicleRaycaster;
+
+enum CarLock : int32_t {
+    CARLOCK_NONE = 0,
+    CARLOCK_UNLOCKED = 1,
+    CARLOCK_LOCKED = 2,
+    CARLOCK_LOCKOUT_PLAYER_ONLY = 3,
+    CARLOCK_LOCKED_PLAYER_INSIDE = 4,
+    CARLOCK_LOCKED_INITIALLY = 5,
+    CARLOCK_FORCE_SHUT_DOORS = 6,
+    CARLOCK_LOCKED_BUT_CAN_BE_DAMAGED = 7,
+};
+
+enum RadioStation : int8_t {
+    HEAD_RADIO = 0x0,
+    DOUBLE_CLEF = 0x1,
+    JAH_RADIO = 0x2,
+    RISE_FM = 0x3,
+    LIPS_106 = 0x4,
+    GAME_FM = 0x5,
+    MSX_FM = 0x6,
+    FLASHBACK = 0x7,
+    CHATTERBOX = 0x8,
+    USERTRACK = 0x9,
+    POLICE_RADIO = 0xA,
+    RADIO_OFF = 0xB,
+};
+
+enum VehicleType {
+    VEHICLE_AUTOMOBILE = 0x0,
+    VEHICLE_BOAT = 0x1,
+    VEHICLE_TRAIN = 0x2,
+    VEHICLE_HELI = 0x3,
+    VEHICLE_PLANE = 0x4,
+    VEHICLE_BIKE = 0x5,
+};
 
 /**
  * @class VehicleObject
@@ -94,6 +129,80 @@ public:
 
     std::unordered_map<std::string, Part> dynamicParts;
 
+    // RE stuff
+    // Please don't modify order, add or remove objects here!
+    // If member is unknown and unused, then it should be also commented
+    // If member is replaced by something else, then you should point/link to
+    // that "something else" using "see ..."
+
+    // CPhysical __parent; // see bullet stuff
+    // tHandlingData *m_pVehicleHandling; // see info
+    // AutoPilot autoPilot;  // CAutoPilot m_sAutoPilot;
+    // uint8_t m_bPrimaryColor; // see colourPrimary
+    // uint8_t m_bSecondaryColor; // see colourSecondary
+    // char m_bVariationA;
+    // char m_bVariationB;
+    // int16_t m_wAlarm;
+    // char field_418;
+    // char field_419;
+    // CPed *m_pDriver; // see seatOccupants
+    // CPed *m_apPassengers[8]; // see seatOccupants
+    // char m_bTotalPassengers; // see seatOccupants
+    // char m_bPeopleAttemptingToEnter; // see seatOccupants
+    // char m_bSeatsBeingEntered; // see seatOccupants
+    // char m_bSeatsTaken; // see seatOccupants
+    // char m_bMaxPassengers; // see seatOccupants
+    // char field_461;
+    // char field_462;
+    // char field_463;
+    // int field_464;
+    // float field_468;
+    // float field_472;
+    // float field_476;
+    // GameObject* CEntity;  // CEntity *m_pCurrentMapObject;
+    // CFire *m_pFire;
+    // float m_fWheelState;
+    // float m_fAccelerationState;
+    // float m_fBrakeState;
+    // char m_nReference;
+    // uint8_t m_bFlags501;
+    // uint8_t m_bFlags502;
+    // uint8_t m_bFlags503;
+    // uint8_t m_bFlags504;
+    // char field_505;
+    // char field_506;
+    // char m_bPacManPickupsCarried;
+    // char field_508;
+    // char field_509;
+    // int16_t m_wRoadblockNode;
+    // float m_fHealth; // see health
+    // uint8_t m_bGearSimpleCar;
+    // char field_517;
+    // char field_518;
+    // char field_519;
+    // float m_fVelocity;
+    // int field_524;
+    // int m_nTimeDestroyed;
+    // int16_t m_nTimeTargetStuckInCar;
+    // int16_t m_nTimeUntilExplosion;
+    // CEntity *m_pDestoyer;
+    // float m_fMapObjectHeightAbove;
+    // float m_fMapObjectHeightBelow;
+    // CarLock carLock;  // eCarLock m_eCarLock;
+    // char m_bLastDamagedWith;
+    // RadioStation radioStation;  // eRadioStation m_eRadioStation;
+    // char m_bRainAudioCounter;
+    // uint8_t m_bRainSamplesCounter;
+    // char m_bHornCounter;
+    // char field_557;
+    // char m_bSirenOrAlarm;
+    // char field_559;
+    // CStoredCollPoly m_sCollPolys[2];
+    // float field_640;
+    // VehicleType vehicleType;  // eVehicleType m_nType;
+
+    // end of RE stuff
+
     VehicleObject(GameWorld* engine, const glm::vec3& pos, const glm::quat& rot,
                   BaseModelInfo* modelinfo, VehicleInfo* info,
                   const glm::u8vec3& prim, const glm::u8vec3& sec);
@@ -131,7 +240,7 @@ public:
 
     void setExtraEnabled(size_t extra, bool enabled);
 
-    void setSteeringAngle(float, bool=false);
+    void setSteeringAngle(float, bool = false);
 
     float getSteeringAngle() const;
 
@@ -232,16 +341,16 @@ public:
 
     /**
      * @brief collectSpecial
-     * @return a positive distance when the point is at the right side of the car
-     * and a negative distance when the point is at the left side of the car
+     * @return a positive distance when the point is at the right side of the
+     * car and a negative distance when the point is at the left side of the car
      */
     float isOnSide(const glm::vec3& point);
-
 
     void grantOccupantRewards(CharacterObject* character);
 
     /**
-     * @return The position, and size of the area that must be free for the vehicle to continue.
+     * @return The position, and size of the area that must be free for the
+     * vehicle to continue.
      */
     std::tuple<glm::vec3, glm::vec3> obstacleCheckVolume() const;
 
